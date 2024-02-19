d146eab82c14d4a2886ff92a6794b90476f3c926
sayyidazizii <sayyidsyafiq234@gmail.com>
2024-02-19 14:51:02 +0700

    update dashboard

--- application/views/home.php
+++ application/views/home.php
@@ -21,14 +21,14 @@
             <div class="col-lg-4 col-md-4 col-sm-12">
               <div class="card card-statistic-2">
                 <div class="card-icon shadow-primary bg-primary">
-                  <i class="fas fa-shopping-bag"></i>
+                  <i class="fas fa-exchange-alt"></i>
                 </div>
                 <div class="card-wrap">
                   <div class="card-header">
-                    <h4>Pembelian</h4>
+                    <h4>Mutasi Barang</h4>
                   </div>
                   <div class="card-body">
-                    $187,13
+                    10
                   </div>
                 </div>
               </div>
@@ -43,7 +43,7 @@
                     <h4>Penjualan</h4>
                   </div>
                   <div class="card-body">
-                    4,732
+                    15
                   </div>
                 </div>
               </div>
