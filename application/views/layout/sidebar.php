be1bc99ae8696848bd7b1a153b67f8a457136b77
sayyidazizii <sayyidsyafiq234@gmail.com>
2024-02-19 14:42:33 +0700

    sidebar active

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -1,3 +1,16 @@
+<?php
+// Mendapatkan segment terakhir dari URL
+$current_url_segment = $this->uri->segment($this->uri->total_segments());
+
+// Daftar segment yang sesuai dengan setiap sidebar
+$sidebar_segments = array(
+    'Home' => 'Dashboard',
+    'Item' => 'Persediaan',
+    'ItemStock' => 'Persediaan',
+    'SalesInvoice' => 'Penjualan',
+    'User' => 'Utility',
+);
+?>
 <div class="main-sidebar sidebar-style-2">
   <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
@@ -8,33 +21,33 @@
     </div>
     <ul class="sidebar-menu">
       <li class="menu-header">Dashboard</li>
-      <li class="dropdown active">
+      <li class="dropdown <?php echo ($sidebar_segments[$current_url_segment] == 'Dashboard') ? 'active' : ''; ?>">
         <a href="#" class="nav-link has-dropdown"><i class="fas fa-home"></i><span>Dashboard</span></a>
         <ul class="dropdown-menu">
-          <li class=active>
-            <a class="nav-link" href="<?php echo base_url() ?>Home">Dashboard</a>
+          <li class="<?php echo ($current_url_segment == 'Home') ? 'active' : ''; ?>">
+            <a class="nav-link" href="<?php echo base_url() ?>Home">Home</a>
           </li>
         </ul>
       </li>
       <li class="menu-header">Menu</li>
-      <li class="dropdown">
+      <li class="dropdown <?php echo ($sidebar_segments[$current_url_segment] == 'Persediaan') ? 'active' : ''; ?>">
         <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
           <span>Persediaan</span></a>
         <ul class="dropdown-menu">
-          <li><a class="nav-link" href="<?php echo base_url() ?>Item">Barang</a></li>
-          <li><a class="nav-link" href="<?php echo base_url() ?>ItemStock">Mutasi Stock</a></li>
+          <li><a class="nav-link <?php echo ($current_url_segment == 'Item') ? 'active' : ''; ?>" href="<?php echo base_url() ?>Item">Barang</a></li>
+          <li><a class="nav-link <?php echo ($current_url_segment == 'ItemStock') ? 'active' : ''; ?>" href="<?php echo base_url() ?>ItemStock">Mutasi Stock</a></li>
         </ul>
       </li>
-      <li class="dropdown">
+      <li class="dropdown <?php echo ($sidebar_segments[$current_url_segment] == 'Penjualan') ? 'active' : ''; ?>">
         <a href="#" class="nav-link has-dropdown"><i class="fas fa-shopping-cart"></i> <span>Penjualan</span></a>
         <ul class="dropdown-menu">
-          <li><a class="nav-link" href="<?php echo base_url() ?>SalesInvoice">Penjualan</a></li>
+          <li><a class="nav-link <?php echo ($current_url_segment == 'SalesInvoice') ? 'active' : ''; ?>" href="<?php echo base_url() ?>SalesInvoice">Penjualan</a></li>
         </ul>
       </li>
-      <li class="dropdown">
+      <li class="dropdown <?php echo ($sidebar_segments[$current_url_segment] == 'Utility') ? 'active' : ''; ?>">
         <a href="#" class="nav-link has-dropdown"><i class="fas fa-cog"></i> <span>Utility</span></a>
         <ul class="dropdown-menu">
-          <li><a href="<?php echo base_url() ?>User">Pengguna</a></li>
+          <li><a class="nav-link <?php echo ($current_url_segment == 'User') ? 'active' : ''; ?>" href="<?php echo base_url() ?>User">Pengguna</a></li>
           <li><a href="auth-reset-password.html">Reset Password</a></li>
         </ul>
       </li>
