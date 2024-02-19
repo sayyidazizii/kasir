730bf4c65524523c4d99018354eb48ea9f1fb0b2
sayyidazizii <sayyidsyafiq234@gmail.com>
2024-02-19 16:05:39 +0700

    add index home

--- application/views/pages/sales_invoice/index.php
+++ application/views/pages/sales_invoice/index.php
@@ -5,7 +5,7 @@
                 <?= $title ?>
             </h1>
             <div class="section-header-breadcrumb">
-                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                <div class="breadcrumb-item active"><a href="<?= base_url()?>Home">Dashboard</a></div>
                 <div class="breadcrumb-item">
                     <?= $title ?>
                 </div>
